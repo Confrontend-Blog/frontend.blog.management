b2adc46da50d63b5d8b84b162ec76d0d237c8fb2
Confrontend <hamed.fatehi.66@gmail.com>
2023-10-16 08:46:14 -0600

    Comment

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import "./global.css";
 
 import { initWhyDidYouRender } from "./utils/error-handling/why-did-you-render";
 
+// All this because wdyr must get loaded before any other react component
 void (async () => {
   await initWhyDidYouRender();
   await import("./main");
