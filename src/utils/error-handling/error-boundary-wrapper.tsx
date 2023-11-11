461b41d07dc4ae1abca705117650f650acbe0f70
Confrontend <hamed.fatehi.66@gmail.com>
2023-11-11 16:08:09 -0600

    improve error boundary

--- src/utils/error-handling/error-boundary-wrapper.tsx
+++ src/utils/error-handling/error-boundary-wrapper.tsx
@@ -1,8 +1,43 @@
-import { ReactElement } from "react";
+import React, { Component, ReactNode } from "react";
 
-import { ErrorBoundary } from "./error-boundty";
+import logger from "./logger";
 
-const ErrorBoundaryWrapper = ({ children }: { children: ReactElement }) => {
+type Props = {
+  children: ReactNode;
+};
+
+type State = {
+  hasError: boolean;
+  error: Error | null;
+};
+
+class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false, error: null };
+  }
+
+  static getDerivedStateFromError(error: Error): State {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true, error };
+  }
+
+  componentDidCatch(error: Error, errorInfo: React.ErrorInfo) {
+    logger.error(`Error caught by Error Boundary: ${error.toString()}`);
+    logger.debug(`Additional info: ${errorInfo.componentStack}`);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // TODO add custom UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
+const ErrorBoundaryWrapper = ({ children }: { children: ReactNode }) => {
   return <ErrorBoundary>{children}</ErrorBoundary>;
 };
 
